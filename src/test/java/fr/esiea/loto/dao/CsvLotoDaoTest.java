55a593a9da05dd2b78b3a95d13288dda03102538
Avenflar <artefacttt92@gmail.com>
2018-02-16 19:52:27 +0100

    Implémentation du Test

---
+++ src/test/java/fr/esiea/loto/dao/CsvLotoDaoTest.java
@@ -0,0 +1,14 @@
+package fr.esiea.loto.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class CsvLotoDaoTest {
+
+	@Test
+	public void test() {
+
+		Assert.assertEquals(1, 1);
+	}
+}
